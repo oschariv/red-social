bfbc477132e5cab0e01c14b5d86be8e4d29642bb
Oscar Charro Rivera <oschariv@gmail.com>
2017-03-11 14:04:41 +0100

    Creada la clase Muro

---
+++ Muro.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Muro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Muro
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<EntradaTexto> mensajes;
+    private ArrayList<EntradaFoto> fotos;
+
+    /**
+     * Constructor for objects of class Muro
+     */
+    public Muro()
+    {
+
+    }
+
+    /**
+     * 
+     */
+    public void addEntradaTexto(EntradaTexto entradaTexto)
+    {
+
+    }
+
+    /**
+     * 
+     */
+    public void addEntradaFoto(EntradaFoto entradaFoto)
+    {
+
+    }
+
+    /**
+     * 
+     */
+    public String toSTring()
+    {
+        return "";
+    }
+}
