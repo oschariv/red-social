9273bf61101ff258ee44fe8b64a01da7bed66f98
Oscar Charro Rivera <oschariv@gmail.com>
2017-03-11 14:09:26 +0100

    Modificado metodo toString

--- EntradaTexto.java
+++ EntradaTexto.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 /**
  * Write a description of class EntradaTexto here.
@@ -62,7 +63,30 @@ public class EntradaTexto
      */
     public String toString()
     {
-        String cadenaADevolver = null;        
+        String cadenaADevolver = null;
+        
+        cadenaADevolver = "Autor: " + usuario + ". Mensaje: " + mensaje;
+        // Comprobamos si la diferencia de los localdatetime es mayor de 59 segundos.
+        if (momentoPublicacion.until(LocalDateTime.now(), ChronoUnit.SECONDS) <= 59) {
+            cadenaADevolver = cadenaADevolver + " Tiempo transcurrido: " + 
+                    momentoPublicacion.until(LocalDateTime.now(), ChronoUnit.SECONDS)
+                            + " segundos.";
+        }
+        // Si es mayor mostramos los minutos y los segundos.
+        else {
+            cadenaADevolver = cadenaADevolver + " Tiempo transcurrido: " + 
+                momentoPublicacion.until(LocalDateTime.now(), ChronoUnit.MINUTES)
+                + " minutos, " + 
+                (momentoPublicacion.until(LocalDateTime.now(), ChronoUnit.SECONDS) - 60) 
+                + " segundos.";
+        }
+        
+        if (comentarios.isEmpty()) {
+            cadenaADevolver = cadenaADevolver + " - No contiene comentarios";
+        }
+        
+        //TEMPORAL
+        System.out.println(cadenaADevolver);
         return cadenaADevolver;
     }
 }
